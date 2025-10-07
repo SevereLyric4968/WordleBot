a20719a537ea4c7b42191165a2e8cdcb6112b0af
Jay Darroch <jd2043@hw.ac.uk>
2025-10-07 12:13:59 +0100

    read file test

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+def readFile(file):
+    with open(file) as file:
+        for words in file:
+            print(words)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+readFile("wordle-La.txt")
+readFile("wordle-Da.txt")
